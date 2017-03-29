400b93cc00f9fad32a49c9ddccdc049ed10f2a25
Yann Duran <yd@live.com.au>
2017-03-29 13:19:30 +1100

    Update Validation Nuget package to v14 (from v15)

--- src/info/VersionInfo.cs
+++ src/info/VersionInfo.cs
@@ -12,7 +12,7 @@ namespace Luminous.Code
         private const string Major = "0";
         private const string Minor = "2";
         private const string Revision = "0";
-        private const string Build = "3";
+        private const string Build = "4";
 
         private const string RevisionVersion = Major + "." + Minor + "." + Revision;
         private const string BuildVersion = RevisionVersion + "." + Build;
