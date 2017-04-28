5b989c03ee4e305cd7d8c5e9416cbfa4eb909db8
Yann Duran <yd@live.com.au>
2017-04-28 16:53:38 +1000

    Bump version to 1.0.2.2

--- src/info/VersionInfo.cs
+++ src/info/VersionInfo.cs
@@ -12,7 +12,7 @@ namespace Luminous.Code
         private const string Major = "1";
         private const string Minor = "0";
         private const string Revision = "2";
-        private const string Build = "1";
+        private const string Build = "2";
 
         private const string RevisionVersion = Major + "." + Minor + "." + Revision;
         private const string BuildVersion = RevisionVersion + "." + Build;
