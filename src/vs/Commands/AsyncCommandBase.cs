2a41a44eed8e31970688690be2bf3d36024749d6
Yann Duran <yd@live.com.au>
2019-02-16 09:24:41 +1100

    feature(VisualStudio): add AsyncCommandBase

---
+++ src/vs/Commands/AsyncCommandBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Luminous.Code.VisualStudio.Commands
+{
+    using Packages;
+
+    public abstract class AsyncCommandBase
+    {
+        protected static AsyncPackageBase Package { get; private set; }
+
+        protected static int Id { get; private set; }
+
+        protected static AsyncCommandBase Instance { get; set; }
+
+        protected AsyncCommandBase(AsyncPackageBase package, int id)
+        {
+            Package = package ?? throw new ArgumentNullException(nameof(package));
+            Id = id;
+        }
+    }
+}
