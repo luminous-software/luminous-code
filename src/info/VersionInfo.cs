14e55fecefb5d7efd4b4b771e5c076b0e764e2f3
Yann Duran <yd@live.com.au>
2019-03-16 10:31:57 +1100

    refactor(VersionInfo): bump version number to v1.1.0.9

--- src/info/VersionInfo.cs
+++ src/info/VersionInfo.cs
@@ -12,7 +12,7 @@ namespace Luminous.Code
         private const string Major = "1";
         private const string Minor = "1";
         private const string Revision = "0";
-        private const string Build = "8";
+        private const string Build = "9";
 
         private const string RevisionVersion = Major + "." + Minor + "." + Revision;
         private const string BuildVersion = RevisionVersion + "." + Build;
